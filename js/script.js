08878a54166a1d1f35cc3035c8adcbcce4c6c9ed
Choton Mohammad Ishaque <chotonmohammed@gmail.com>
2022-04-12 15:06:20 +0600

    first release

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(() => {
+  $('a[href*="#"]:not([href="#"])').click((e) => {
+    const target = $(e.target.hash);
+
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+
+      return false;
+    }
+  });
+});
+
+///slider carousel
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
